1a6b8f12fe17f7b9dd07a88b7e07d8a258dcd611
reid <reidy@synnex.com>
2020-09-19 14:57:20 +0800

    some enhance

---
+++ cloud-gateway-9527/src/main/java/com/reidy/gateway/GateWay9527.java
@@ -0,0 +1,17 @@
+package com.reidy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Reid
+ * @Date 2020/9/17
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWay9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWay9527.class);
+    }
+}
